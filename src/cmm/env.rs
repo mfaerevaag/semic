4f9f2f607e3478ab02d669e73ba6cd20ff8eb03b
Markus Faerevaag <m@frv.ag>
2017-11-25 14:00:14 +0900

    Print floats as gcc

--- src/cmm/env.rs
+++ src/cmm/env.rs
@@ -158,7 +158,7 @@ impl Debug for SymVal {
         use self::SymVal::*;
         match *self {
             Int(i) => write!(fmt, "{}", i),
-            Float(f) => write!(fmt, "{:.2}", f),
+            Float(f) => write!(fmt, "{:.5}", f),
             Char(c) => write!(fmt, "'{}'", c),
             Bool(b) => write!(fmt, "{}", b),
             Array(ref a) => write!(fmt, "{:?}", a),
